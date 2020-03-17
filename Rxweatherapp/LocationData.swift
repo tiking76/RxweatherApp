6bd82ea66d60eb70a93d53c806d011884db5ad7a
tiking76 <716yosiki1999@gmail.com>
2020-03-17 18:40:27 +0900

    add imgefile

--- Rxweatherapp/LocationData.swift
+++ Rxweatherapp/LocationData.swift
@@ -8,6 +8,18 @@
 
 import Foundation
 
-enum LocationData {
-    
+enum LocationData: String {
+    case tokyo = "tokyo"
+    case fukushima = "fukushima"
+    case osaka = "osaka"
+    case kyoto = "kyoto"
 }
+
+enum Weather : String {
+    case sunny = "晴れ"
+    case cloudy = "曇り"
+    case rain = "雨"
+    case snow = "雪"
+}
+
+
