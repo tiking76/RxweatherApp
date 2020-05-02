e0210cbdaa26466eda759c96fa94f68ca3e99bc0
tiking76 <716yosiki1999@gmail.com>
2020-05-03 03:30:26 +0900

    とりあえず

--- Rxweatherapp/Model/APIFormat.swift
+++ Rxweatherapp/Model/APIFormat.swift
@@ -76,22 +76,40 @@ struct Sys : Codable {
 
 class NetworkingClient {
     
-  func getAddress() {
-    let text = baseURL + location + ",jp&units=metric&APPID=" + myAPIKey
-    let lowurl = text.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
     
-    //ここからデータ通信している
-    Alamofire.request(lowurl, method: .get, parameters: nil, encoding: JSONEncoding.default)
-    .responseJSON { (response) in
-      switch response.result {
-        //成功時に実行する内容
-          case .success:
-            print(response)
-        //エラー処理
-          case let .failure(error):
-            print(error)
-      }
+   func getAddress() {
+        let text = baseURL + location + ",jp&units=metric&APPID=" + myAPIKey
+        let lowurl = text.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+        //ここからデータ通信している
+        Alamofire.request(lowurl, method: .get, parameters: nil, encoding: JSONEncoding.default)
+            .responseJSON { (response) in
+                switch response.result {
+                //成功時に実行する内容
+                case .success:
+                    //多分ここでJSONからオブジェクトに変換されている？
+                    guard let data = response.data else { return }
+                    print(data)
+                    print(type(of: data.self))
+                    let decoder = JSONDecoder()
+                    print("hoge")
+                    guard let weatherResult = try? decoder.decode(DataFormat.self, from: data) else { return }
+                    print(weatherResult)
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                //エラー処理
+                case let .failure(error):
+                    print(error)
+                }
+        }
     }
-  }
 }
+
+
 
