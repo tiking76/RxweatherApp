582706cef9e0333975111170718aa29366a65e50
tiking76 <716yosiki1999@gmail.com>
2020-11-10 22:22:15 +0900

    remove unnecessary self

--- Rxweatherapp/ViewController.swift
+++ Rxweatherapp/ViewController.swift
@@ -32,52 +32,48 @@ class ViewController: UIViewController {
 
         tokyoButton.rx.tap
                 .subscribe(onNext: { [weak self] in
-                    guard let self = self else { return }
-                    self.client.getAddress(LocationData.tokyo.rawValue)
+                    self?.client.getAddress(LocationData.tokyo.rawValue)
                     { format in
-                        self.weatherView.image = UIImage(named: format.weather[0].main)
+                        self?.weatherView.image = UIImage(named: format.weather[0].main)
                         print(format.weather[0].main)
                         DispatchQueue.main.async {
-                            self.weatherView.image = UIImage(named: "Rain")
+                            self!.weatherView.image = UIImage(named: "Rain")
                         }
                     }
                     //self.weatherView.image = UIImage(named: "Rain")
                     //print(self.weatherView.image ?? "hoge")
-                    self.view.addSubview(self.weatherView)
+                    self!.view.addSubview(self!.weatherView)
                 })
                 .disposed(by: disposedBeg)
 
         osakaButton.rx.tap
                 .subscribe(onNext : { [weak self] in
-                    guard let self = self else { return }
-                    self.client.getAddress(LocationData.osaka.rawValue)
+                    self?.client.getAddress(LocationData.osaka.rawValue)
                     { format in
-                        self.weatherView.image = UIImage(named: format.weather[0].icon)
+                        self?.weatherView.image = UIImage(named: format.weather[0].icon)
                     }
-                    self.weatherView.image = UIImage(named: self.client.weatherIcon)
-                    self.view.addSubview(self.weatherView)
+                    self!.weatherView.image = UIImage(named: (self?.client.weatherIcon)!)
+                    self?.view.addSubview(self!.weatherView)
                 })
                 .disposed(by: disposedBeg)
 
         kyotoButton.rx.tap
                 .subscribe(onNext : { [weak self] in
-                    guard let self = self else { return }
-                    self.client.getAddress(LocationData.kyoto.rawValue)
+                    self?.client.getAddress(LocationData.kyoto.rawValue)
                     { format in
-                        self.weatherView.image = UIImage(named: format.weather[0].icon)
+                        self?.weatherView.image = UIImage(named: format.weather[0].icon)
                     }
-                    self.view.addSubview(self.weatherView)
+                    self?.view.addSubview((self?.weatherView)!)
                 })
                 .disposed(by: disposedBeg)
 
         fukushimaButton.rx.tap
                 .subscribe(onNext :{ [weak self] in
-                    guard let self = self else { return }
-                    self.client.getAddress(LocationData.fukushima.rawValue)
+                    self?.client.getAddress(LocationData.fukushima.rawValue)
                     { format in
-                        self.weatherView.image = UIImage(named: format.weather[0].icon)
+                        self?.weatherView.image = UIImage(named: format.weather[0].icon)
                     }
-                    self.view.addSubview(self.weatherView)
+                    self?.view.addSubview((self?.weatherView)!)
                 })
                 .disposed(by: disposedBeg)
     }
